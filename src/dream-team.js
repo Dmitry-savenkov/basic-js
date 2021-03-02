3df67ac2d3216c87d8fb4f259a4dd52223c7801b
Dmitry_Savenkov <73306821+Dmitry-savenkov@users.noreply.github.com>
2021-03-02 18:12:02 +0300

    Update dream-team.js

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,15 +1,16 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(array) {
+module.exports = function createDreamTeam(members) {
   if(!Array.isArray(members)) {
     return false
   }
-  var arr = [];
-  var tempStr = '';
-  for (let i = 0; i < array.length; i++) {
-    if (typeof name === 'string')
-      tempStr = array[i].trim();
-      arr.push(tempStr[0].toUpperCase())
+  let res = []
+  let temp = ''
+  for(let i = 0; i < members.length; i++) {
+    if(typeof members[i] == "string") {
+      temp = members[i].trim()
+      res.push(temp[0].toUpperCase())
     }
-    return String(arr.sort().join(''))
-};
+  }
+  return res.sort().join('')
+}
