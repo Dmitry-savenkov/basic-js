8f2266d47f1df16d21db4e3f9cc720e39c857dc8
Dmitry_Savenkov <73306821+Dmitry-savenkov@users.noreply.github.com>
2021-03-04 17:51:35 +0300

    fix : new solution

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,30 +1,32 @@
 const CustomError = require("../extensions/custom-error");
 
 const chainMaker = {
-  array: [],
+  chain: [],
+  
   getLength() {
-  return this.array.length;
+          return this.chain.length
   },
-  addLink(value) {
-    this.array.push(` ( ${value} ) `);
-    return this;
+  addLink(value = '') {
+  value = value === '' ? `( )` : `( ${value} )`
+         this.chain.push(value)
+         return this
   },
-  removeLink(PosNumber) {
-    if (typeof PosNumber != 'number' || PosNumber < 1 || PosNumber > this.array.length) {
-      this.array = [];
-      throw new Error('Error');
-    }
-    this.array.splice(PosNumber - 1, 1);
-    return this;
+  removeLink(position) {
+    if (!parseInt(position) || isNaN(parseInt(position)) || this.chain[position] === undefined) {
+        this.chain = []
+        throw new Error()
+  }
+        this.chain.splice(position - 1, 1)
+        return this
   },
   reverseChain() {
-    this.array.reverse();
-    return this;
+        this.chain.reverse()
+        return this
   },
   finishChain() {
-    let string = this.array.join('~~');
-    this.array = [];
-    return string;
+        let chained = this.chain.join('~~');
+        this.chain = []
+        return chained
   }
 };
 
