b24f9597fbba2d9dd79eea130070ff30812ac4e0
Dmitry_Savenkov <73306821+Dmitry-savenkov@users.noreply.github.com>
2021-03-05 01:35:16 +0300

    Update vigenere-cipher.js

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,13 +1,44 @@
 const CustomError = require("../extensions/custom-error");
 
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  
+  constructor(type) {
+    
+    this.type = arguments.length ? type : true;
+    
+    this.charCount = 26;
+    
+  }
+  
+  crypt(less, key, operation) {
+    
+    let WordTemp = "";
+    let Poskey = 0;
+    
+    for (let i = 0; i < less.length; i++) {
+      
+      let m = less.toUpperCase().charCodeAt(i) - 65;
+      if (m < 0 || m > this.charCount) {
+        WordTemp += less[i];
+        
+        if (m != -33) Poskey += 1;
+        
+        continue;
+      }
+      let k = key.toUpperCase().charCodeAt(Poskey++ % key.length) - 65;
+      
+      WordTemp += String.fromCharCode((operation(m, k) % this.charCount) + 65);
+    }
+    if (!this.type) WordTemp = WordTemp.split("").reverse().join("");
+    return WordTemp;
+  }
+  
+  encrypt(less, key) {
+    return this.crypt(less, key, (m, k) => m + k);
+  }
+  
+  decrypt(less, key) {
+    return this.crypt(less, key, (m, k) => m + this.charCount - k);
   }
 }
 
