28f6bc205676a3d2b17d63281cb8b8d3d6188abe
Dmitry_Savenkov <73306821+Dmitry-savenkov@users.noreply.github.com>
2021-03-04 17:40:48 +0300

    fix : array problem

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,25 +1,30 @@
 const CustomError = require("../extensions/custom-error");
 
 const chainMaker = {
+  array: [],
   getLength() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  return this.array.length;
   },
   addLink(value) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    this.array.push(` ( ${value} ) `);
+    return this;
   },
-  removeLink(position) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  removeLink(PosNumber) {
+    if (typeof PosNumber != 'number' || PosNumber < 1 || PosNumber > this.array.length) {
+      this.array = [];
+      throw new Error('Error');
+    }
+    this.array.splice(PosNumber - 1, 1);
+    return this;
   },
   reverseChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    this.array.reverse();
+    return this;
   },
   finishChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    let string = this.array.join('~~');
+    this.array = [];
+    return string;
   }
 };
 
